dfa0c7e9377ae087a9bde3c469af8d970b400890
SpeedOfMagic <davasilev_4@edu.hse.ru>
2023-02-12 21:06:51 +0300

    Deployed contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { PRIVATE_KEY, API_KEY } = process.env;
+module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: { 
+      url: `https://eth-goerli.alchemyapi.io/v2/${API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
-
-export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,10 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const Coin20 = await ethers.getContractFactory("Coin20");
+  const coin20 = await Coin20.deploy();
+  await coin20.deployed();
+  console.log(`Coin20 deployed to ${coin20.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
